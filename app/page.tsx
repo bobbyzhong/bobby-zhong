dd6b07ed6e475781f754d0ad39fd6ccd11089829
Bobby Zhong <107908524+bobbyzhong@users.noreply.github.com>
2023-01-24 22:47:19 -0800

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+// download headwind extension to autosort classnames so we see the same thing
+export default function Home() {
+  return (
+    <div className="">
+      <div>If you are here, we got a new client BABY! </div>
+    </div>
+  )
+}
